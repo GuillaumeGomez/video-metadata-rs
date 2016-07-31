04fa3a3d24e5dc35dc622518dac413e9398bc998
Guillaume Gomez <guillaume1.gomez@gmail.com>
2016-07-31 02:16:27 +0200

    No need for pkg-config for windows

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 extern crate gcc;
+#[cfg(not(target_os = "windows"))]
 extern crate pkg_config;
 
+#[cfg(not(target_os = "windows"))]
 use std::env;
+#[cfg(not(target_os = "windows"))]
 use std::fs;
 
+#[cfg(not(target_os = "windows"))]
 fn test_dirs(dir_names: &[&str]) -> Option<String> {
     for dir_name in dir_names {
         match fs::metadata(dir_name) {
@@ -14,6 +18,7 @@ fn test_dirs(dir_names: &[&str]) -> Option<String> {
     None
 }
 
+#[cfg(not(target_os = "windows"))]
 fn look_for_libs() {
     let lib_dir = match env::var("FFMPEG_LIB_DIR") {
         Ok(dir) => dir,
@@ -40,6 +45,9 @@ fn look_for_libs() {
     println!("cargo:rustc-link-lib=libavutil");
     println!("cargo:rustc-link-search={}", lib_dir);
 }
+
+#[cfg(target_os = "windows")]
+fn look_for_libs() {}
 
 fn main() {
     look_for_libs();
